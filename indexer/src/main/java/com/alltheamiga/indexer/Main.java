b9f1cdb3369234d2cd7ddd673d947abf1887a01d
Paul René Jørgensen <paulrene@comoyo.com>
2013-05-29 23:00:25 +0200

    Changing layout.

---
+++ indexer/src/main/java/com/alltheamiga/indexer/Main.java
@@ -0,0 +1,18 @@
+package com.alltheamiga.indexer;
+
+/**
+ * 
+ * Storage Main
+ * 
+ * @author paulrene
+ *
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		
+//		PhysicalVolumeFactory.readDoubleDensityDisk(input)
+		
+	}
+
+}
